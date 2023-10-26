9b972337278ca51689fba26f21cda579958c9858
yiting-tom <liyitingtom@gmail.com>
2023-10-26 14:50:31 +0800

    add: tasks module

---
+++ tasks/nlu_tasks/__init__.py
@@ -0,0 +1,7 @@
+from .cola import COLATask
+from .mnli import MNLITask
+from .mrpc import MRPCTask
+from .qnli import QNLITask
+from .qqp import QQPTask
+from .rte import RTETask
+from .sst2 import SST2Task
