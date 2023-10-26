4df41536e3e2e382845db77910394fccbb9dde68
yiting-tom <liyitingtom@gmail.com>
2023-10-26 14:47:54 +0800

    add: models

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from .ofa import (
+    OFAModel,
+    ofa_base_architecture,
+    ofa_huge_architecture,
+    ofa_large_architecture,
+)
---
+++ models/taming/modules/losses/__init__.py
@@ -0,0 +1,1 @@
+from models.taming.modules.losses.vqperceptual import DummyLoss
